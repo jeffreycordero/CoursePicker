2d028607eb32115d98eff0a90bf92b5b0d791090
Jeffrey Cordero <jeffreycordero@outlook.com>
2017-04-11 01:04:37 -0500

    Removing Graphics Code
    Unnecessicary for server code.

--- core/src/main/java/com/Main.java
+++ core/src/main/java/com/Main.java
@@ -15,14 +15,6 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception{
         final FXMLLoader guiLoader = new FXMLLoader(getClass().getResource("CoursePickerGUI.fxml"));
-        this.root = guiLoader.load();
-        primaryStage.setScene(new Scene(root, 1000, 700));
-        primaryStage.setTitle("Course Picker 2.0");
-        primaryStage.setResizable(false);
-        primaryStage.show();
-        CoursePickerGUIController controller = guiLoader.getController();
-        controller.init(primaryStage);
-        // show it
     }
     public static void main(String[] args) {
         launch(args);
